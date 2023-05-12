fd3fdd7ce6100e0680f4d5f9407ba60ed2926eb0
Jaymoo87 <114165259+Jaymoo87@users.noreply.github.com>
2023-05-12 12:30:06 -0500

    openai API still not responding correctley, 429 error... annoying

--- app/api/openai/route.ts
+++ app/api/openai/route.ts
@@ -1,30 +1,44 @@
 import { NextResponse } from 'next/server';
 import { AxiosResponse } from 'axios';
 
+import Bottleneck from 'bottleneck';
+
 import { Configuration, CreateChatCompletionResponse, OpenAIApi } from 'openai';
 
 const configuration = new Configuration({
+  organization: 'org-Y640fG1Ak6SAiqjyYF3R8AS4',
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
+const limiter = new Bottleneck({
+  maxConcurrent: 1, // Only allow 1 request at a time
+  minTime: 2000, // Wait at least 2 seconds between requests
+  reservoir: 3, // Allow up to 3 requests in the reservoir
+  reservoirRefreshInterval: 60 * 1000, // Refresh the reservoir every minute
+});
+
 export async function POST(request: Request) {
   try {
     const { title, role } = await request.json();
-    const aiRes: AxiosResponse<CreateChatCompletionResponse, any> = await openai.createChatCompletion({
-      model: 'gpt-3.5-turbo',
-      messages: [
-        {
-          role: 'user',
-          //   content: `Create small blog post with html tags based on this title: ${title}`,
-          content: `Create a 3 line blog post with html tags based on this title: ${title}`,
-        },
-        {
-          role: 'system',
-          content: `${role || 'I am a helpful assistant'}. Write with html tags`,
-        },
-      ],
-    });
+    const aiRes: AxiosResponse<CreateChatCompletionResponse, any> = await limiter.schedule(() =>
+      openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [
+          {
+            role: 'user',
+            //   content: `Create small blog post with html tags based on this title: ${title}`,
+            content: `Create a 3 line blog post with html tags based on this title: ${title}`,
+          },
+          {
+            role: 'system',
+            content: `${role || 'I am a helpful assistant'}. Write with html tags`,
+          },
+        ],
+        temperature: 5,
+        max_tokens: 3,
+      })
+    );
     return NextResponse.json(
       {
         content: aiRes.data.choices[0].message?.content,
